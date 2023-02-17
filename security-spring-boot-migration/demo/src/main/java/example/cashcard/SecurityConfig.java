a478e107e9e8df50bd5a2b8a4e5823b68e3ca483
Felipe <felipeg@gmail.com>
2023-02-17 09:53:50 -0500

    initial commit - challenge/lab

--- security-spring-boot-migration/demo/src/main/java/example/cashcard/SecurityConfig.java
+++ security-spring-boot-migration/demo/src/main/java/example/cashcard/SecurityConfig.java
@@ -1,15 +1,32 @@
 package example.cashcard;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,12 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserDetails sarah = User.withDefaultPasswordEncoder()
-                .username("sarah1")
-                .password("abc123")
-                .roles("CARD-OWNER")
-                .build();
-        UserDetails kumar =User.withDefaultPasswordEncoder()
+
+        UserDetails kumar = User.withDefaultPasswordEncoder()
                 .username("kumar2")
                 .password("xyz789")
                 .roles("CARD-OWNER")
@@ -42,6 +55,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("non-owner3")
                 .roles("SOME-OTHER-ROLE")
                 .build();
-        auth.inMemoryAuthentication().withUser(sarah).withUser(kumar).withUser(outsider);
+
+        auth.jdbcAuthentication()
+                .withDefaultSchema()
+                .dataSource(dataSource)
+                .withUser(kumar).withUser(outsider);
+        auth.authenticationProvider(new AuthenticationProvider() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String username = authentication.getName();
+                String password = authentication.getCredentials().toString();
+                if ("sarah1".equals(username) && "abc123".equals(password)) {
+                    return new UsernamePasswordAuthenticationToken
+                            (username, password, new ArrayList() {{
+                                add(new SimpleGrantedAuthority("ROLE_CARD-OWNER"));
+                            }});
+                } else {
+                    throw new
+                            BadCredentialsException("External system authentication failed");
+                }
+            }
+
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return authentication.equals(UsernamePasswordAuthenticationToken.class);
+            }
+        });
     }
 }
